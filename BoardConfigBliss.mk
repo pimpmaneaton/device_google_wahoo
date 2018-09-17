4d8fca0db9b901c4e5dce3ab769886d32990bd0b
pimpmaneaton <shaunsibu@gmail.com>
2018-09-17 10:33:00 -0600

    Enable higher aspect ratio option

--- BoardConfigBliss.mk
+++ BoardConfigBliss.mk
@@ -14,12 +14,15 @@
 # limitations under the License.
 #
 
+TARGET_KERNEL_CLANG_TRIPLE := aarch64-linux-gnu-
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := 7.0.2
 TARGET_KERNEL_SOURCE := kernel/google/wahoo
 TARGET_KERNEL_CONFIG := custom_defconfig
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 
 # Audio
 BOARD_SUPPORTS_SOUND_TRIGGER := true
